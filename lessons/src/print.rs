df41ae700a0f024257bec8e222c54b170d12136b
kontactr <gandhiyash1381997@gmail.com>
2019-10-26 12:31:20 +0530

    changed

--- lessons/src/print.rs
+++ lessons/src/print.rs
@@ -7,5 +7,5 @@ pub fn run(){
     println!("{name} , {name1}", name = "A"  , name1 = "B");
     println!("Binary {:b} Hex {:x} Octal {:o}" , 10 , 11 ,  12);
     println!("{:?}",(1,2,3,4,true,5,6,7 , "L"));
-    println!("{}" , 20 + 20 * 20 - 20 / 20 + 50 - 50);
+    println!("{}" , 20 + 20 * 20 - 20 / 20 + 50 - 50 - 60);
 }
